52ef82f68467b244c117632a00ff2b8d4f8ad067
Craig Chambers <cchambers@salisbury6c.ac.uk>
2015-07-31 18:25:34 +0100

    Hid local variables and methods

--- health.py
+++ health.py
@@ -1,17 +1,12 @@
-__author__ = 'craigchambers'
+__author__ = 'Craig Chambers'
 import bmi as b
 
-height = input('Enter your height in inches')
-weight = input('Enter your weight in pounds')
+height = input('Enter your height in inches: ')
+weight = input('Enter your weight in pounds: ')
 
 bmi = b.BMI(int(height), int(weight), 'imperial')
 
-print(bmi.__doc__)
+#print(bmi.__doc__)
 
 print('Your BMI is: {0}'.format(str(bmi.bmi)))
-bmi.height = 50
-print('Changed height to {}'.format(bmi.height))
-bmi.weight = 200
-print('Changed weight to {}'.format(bmi.weight))
-print('Your BMI is: {0}'.format(str(bmi.bmi)))
-
+print("You are currently '{}'".format(bmi.bmi_as_string))
