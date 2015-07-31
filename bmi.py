6b99bb6ff948f6fd380bbffd4d4ff3e431155311
Craig Chambers <cchambers@salisbury6c.ac.uk>
2015-07-31 09:55:27 +0100

    Change to properties for the bmi class

--- bmi.py
+++ bmi.py
@@ -1,11 +1,12 @@
-__author__ = 'craigchambers'
+__author__ = 'Craig Chambers'
+
 
 class BMI:
     '''
     Calculates BMI values for a given weight and height
     '''
 
-    def __init__(self, height, weight, measurement):
+    def __init__(self, height=0, weight=0, measurement='imperial'):
         '''
         Initiates the class with height, weight and the measurement used
         :param height: height of the person in inches oe meters
@@ -13,37 +14,84 @@ class BMI:
         :param measurement: imperial or metric
         :return: none
         '''
-        self.measurement = measurement
-        self.height = height
-        self.weight = weight
-        self.bmi = 0
+        self._measurement = measurement
+        self._height = height
+        self._weight = weight
+        self._bmi = 0
 
-        if self.measurement == 'imperial':
-            self.bmi = self.calcImperialBMI(self.height,self.weight)
-        elif self.measurement == 'metric':
-            self.bmi = self.calcMetricBMI(self.height, self.weight)
+        if self._measurement == 'imperial':
+            self.calc_imperial_bmi(self._height,self._weight)
+        elif self._measurement == 'metric':
+            self.calc_metric_bmi(self._height, self._weight)
         else:
-            raise ValueError('Measurement was not "imperial" or "metric"', self.measurement)
+            raise ValueError('Measurement was not "imperial" or "metric"', self._measurement)
 
+    @property
+    def height(self):
+        '''
+        Height property (used to calculate bmi)
 
-    def calcImperialBMI(self, height, weight):
+        :return: height
+        '''
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        self._height = value
+
+    @property
+    def weight(self):
+        '''
+        Height property (used to calculate bmi)
+
+        :return: weight
+        '''
+        return self._weight
+
+    @weight.setter
+    def weight(self, value):
+        self._weight = value
+        self.which_calculation()
+
+    @property
+    def bmi(self):
+        '''
+        Calculated bmi value as an integer
+        :return: bmi
+        '''
+        return int(self._bmi)
+
+    @bmi.setter
+    def bmi(self, value):
+        self._bmi = value
+        self.which_calculation()
+
+    def which_calculation(self):
+        if self._measurement == 'imperial':
+            self.calc_imperial_bmi(self._height, self._weight)
+        elif self._measurement == 'metric':
+            self.calc_metric_bmi(self._height, self._weight)
+        else:
+            raise ValueError('Measurement was not "imperial" or "metric"', self._measurement)
+
+    def calc_imperial_bmi(self, the_height, the_weight):
         '''
         Calculates BMI in Imperial measurements
+
         English BMI Formula
             BMI = ( Weight in Pounds / ( Height in inches x Height in inches ) ) x 703
-        :param height: height in inches
-        :param weight: weight in pounds
-        :return: BMI value
+        :param the_height: height in inches
+        :param the_weight: weight in pounds
         '''
-        return (weight/(height*height))* 703
+        self._bmi = the_weight/(the_height*the_height) * 703
 
-    def calcMetricBMI(self, height, weight):
+    def calc_metric_bmi(self, the_height, the_weight):
         '''
         Calculates BMI in metric measurements
+
         Metric BMI Formula
             BMI = ( Weight in Kilograms / ( Height in Meters x Height in Meters ) )
-        :param height: height in meters
-        :param weight: weight in kilograms
-        :return: BMI value
+        :param the_height: height in meters
+        :param the_weight: weight in kilograms
         '''
-        return (weight/(height*height))
+        self._bmi = the_weight/(the_height*the_height)
--- health.py
+++ health.py
@@ -5,4 +5,13 @@ height = input('Enter your height in inches')
 weight = input('Enter your weight in pounds')
 
 bmi = b.BMI(int(height), int(weight), 'imperial')
+
+print(bmi.__doc__)
+
 print('Your BMI is: {0}'.format(str(bmi.bmi)))
+bmi.height = 50
+print('Changed height to {}'.format(bmi.height))
+bmi.weight = 200
+print('Changed weight to {}'.format(bmi.weight))
+print('Your BMI is: {0}'.format(str(bmi.bmi)))
+
