5e404817598e6717b556495572b6b472f9fab5e4
Craig Chambers <cchambers@salisbury6c.ac.uk>
2015-08-05 14:47:36 +0100

    Added conversion to metric so BMR can be calculated

--- health.py
+++ health.py
@@ -10,10 +10,15 @@ def input_as_integer(value):
     except ValueError:
         print('You must input a number')
 
-def convert_pounds_to_kilograms():
-    pass
 
-def convert_inches_to_centimetres():
+def convert_pounds_to_kilograms(weight_in_pounds):
+    # One kilogram is equal to 2.2046226218 pounds
+    return weight_in_pounds / 2.2046226218
+
+
+def convert_inches_to_centimetres(length_in_inches):
+    # 1 inch = 2.54
+    return length_in_inches * 2.54
     pass
 
 person = p.Person()
@@ -23,10 +28,10 @@ person.weight = input_as_integer('Enter your weight in pounds: ')
 person.age = input_as_integer('Enter your age in years: ')
 
 bmi_result = bmi.BMI(int(person.height), int(person.weight), 'imperial')
-bmr_result = bmr.BMR(person.sex, person.weight, person.height, person.age)
+bmr_result = bmr.BMR(person.sex, convert_pounds_to_kilograms(person.weight), convert_inches_to_centimetres(person.height), person.age)
 
 #print(bmi.__doc__)
 
 print('Your BMI is: {0}'.format(str(bmi_result.bmi)))
 print("You are currently '{}'".format(bmi_result.bmi_as_string))
-print('Your BMR is: {}'.format(str(bmr_result.bmr)))
+print('Your BMR is: {}'.format(str(int(bmr_result.bmr))))
