e7ab9f28c11b8e27048f9a9115157132c6575577
Craig Chambers <cchambers@salisbury6c.ac.uk>
2015-08-08 08:53:51 +0100

    Updates to meet PEP8

--- bmi.py
+++ bmi.py
@@ -2,23 +2,23 @@ __author__ = 'Craig Chambers'
 
 
 class BMI:
-    '''
+    """
     Calculates BMI values for a given weight and height
 
     Defaults to:
      height=0
      weight=0
      measurement = 'imperial'
-    '''
+    """
 
     def __init__(self, height=0, weight=0, measurement='imperial'):
-        '''
+        """
         Initiates the class with height, weight and the measurement used
-        :param height: height of the person in inches oe meters
+        :param height: height of the person in inches or meters
         :param weight: weight of the person in pounds or kilograms
         :param measurement: imperial or metric
         :return: none
-        '''
+        """
         self.__measrement = measurement
         self.__height = height
         self.__weight = weight
@@ -33,11 +33,11 @@ class BMI:
 
     @property
     def height(self):
-        '''
+        """
         Height property (used to calculate bmi)
 
         :return: height
-        '''
+        """
         return self.__height
 
     @height.setter
@@ -46,11 +46,11 @@ class BMI:
 
     @property
     def weight(self):
-        '''
+        """
         Height property (used to calculate bmi)
 
         :return: weight
-        '''
+        """
         return self.__weight
 
     @weight.setter
@@ -60,10 +60,10 @@ class BMI:
 
     @property
     def bmi(self):
-        '''
+        """
         Calculated bmi value as an integer
         :return: bmi
-        '''
+        """
         return int(self.__bmi)
 
     @bmi.setter
@@ -73,11 +73,11 @@ class BMI:
 
     @property
     def bmi_as_string(self):
-        '''
+        """
         Property that shows the bmi property as a string
 
         :return: bmi as a string (underweight, healthy, overweight, obese, extremely obese)
-        '''
+        """
         bmi_string=''
         if self.__bmi < 19:
             bmi_string='Underweight'
@@ -94,11 +94,11 @@ class BMI:
         return bmi_string
 
     def __which_calculation(self):
-        '''
+        """
         Calls relevant function to calculate bmi based on value in self.__measrement (defaults to 'imperial')
 
         :return: none
-        '''
+        """
         if self.__measrement == 'imperial':
             self.__calc_imperial_bmi(self.__height, self.__weight)
         elif self.__measrement == 'metric':
@@ -107,7 +107,7 @@ class BMI:
             raise ValueError('Measurement was not "imperial" or "metric"', self.__measrement)
 
     def __calc_imperial_bmi(self, the__height, the__weight):
-        '''
+        """
         Calculates BMI in Imperial measurements
 
         English BMI Formula
@@ -115,11 +115,11 @@ class BMI:
         :param the__height: height in inches
         :param the__weight: weight in pounds
         :return none
-        '''
+        """
         self.__bmi = the__weight/(the__height*the__height) * 703
 
     def __calc_metric_bmi(self, the__height, the__weight):
-        '''
+        """
         Calculates BMI in metric measurements
 
         Metric BMI Formula
@@ -127,5 +127,5 @@ class BMI:
         :param the__height: height in meters
         :param the__weight: weight in kilograms
         :return none
-        '''
+        """
         self.__bmi = the__weight/(the__height*the__height)
